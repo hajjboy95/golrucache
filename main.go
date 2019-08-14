34aaa1d1a319b8fa3ffbf2a1cd40214eb0e19903
Ismail El-Habbash <gizmo-boy95@hotmail.com>
2019-08-14 22:03:54 +0800

    removes dummy code

--- main.go
+++ main.go
@@ -1,39 +1,25 @@
 package main
 
 import (
-	"container/list"
+	"fmt"
+	"github.com/hajjboy95/lru_cache/lru_cache"
 )
 
 func main() {
-	l := list.New()
-	a := &list.Element{Value: "1"}
-	b := &list.Element{Value: "2"}
-	c := &list.Element{Value: "3"}
-	d := &list.Element{Value: "4"}
-	e := &list.Element{Value: "5"}
+	var cache  = lru_cache.New(4)
+	val := cache.Get("val")
+	fmt.Println("yiked " , val)
 
-	l.PushFront(a)
-	l.PushFront(b)
-	l.PushFront(c)
-	l.PushFront(d)
-	l.PushFront(e)
+	cache.Put("val1", "hi1")
+	cache.Put("val2", "hi2")
+	cache.Put("val3", "hi3")
+	cache.Put("val4", "hi4")
+	cache.Put("val5", "hi5")
+	cache.Put("val6", "hi6")
 
-	//
-	//var cache  = lru_cache.New(4)
-	//
-	//val := cache.Get("val")
-	//fmt.Println("yiked " , val)
-	//
-	//cache.Put("val1", "hi1")
-	//cache.Put("val2", "hi2")
-	//cache.Put("val3", "hi3")
-	//cache.Put("val4", "hi4")
-	//cache.Put("val5", "hi5")
-	//cache.Put("val6", "hi6")
-	//
-	//cache.PrintAll()
-	//a := cache.Get("val4")
-	//fmt.Println(a)
-	//cache.PrintAll()
+	cache.PrintAll()
+	a := cache.Get("val4")
+	fmt.Println(a)
+	cache.PrintAll()
 }
 
